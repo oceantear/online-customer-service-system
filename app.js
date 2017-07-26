7771a1b7f10427ae5627ab684051cb9bdcbeea8c
oceantear <oceantear@gmail.com>
2017-07-26 17:05:51 +0800

    fix bugs

--- app.js
+++ app.js
@@ -67,11 +67,21 @@ io.on('connection', function(socket){
         
         for (var i in SocketIdAvaiableArray){
             var obj = SocketIdAvaiableArray[i];
-            if(obj.SocketId == socket.id){
-                console.log('room owner leave!!!');
-                delete SocketIdAvaiableArray[i];
+            if(obj.RoomOwnerSocketId == socket.id){
+                console.log('room owner leave!!!:  obj.CallerSocketId = ',obj.CallerSocketId);
+                var to = io.sockets.connected[obj.CallerSocketId];
+                console.log('disconnect: to = ', obj.CallerSocketId);
+                if(obj.CallerSocketId){
+                    to.emit('leave', obj.CallerSocketId);
+                    
+                }
+                //delete SocketIdAvaiableArray[i];
+                SocketIdAvaiableArray.splice(i,1);
+                break;
             }else if(obj.RoomId == room){
                 SocketIdAvaiableArray[i].Avaliable = 1;
+                SocketIdAvaiableArray[i].CallerSocketId = null;
+                break;
             }
         }
     }
@@ -84,42 +94,62 @@ io.on('connection', function(socket){
         socket.join(name);
         socket.room = name;
     });*/
+    
+  function checkRoomIdDuplicated(name){
+      for (var i in SocketIdAvaiableArray){
+          var obj = SocketIdAvaiableArray[i];
+          console.log('obj.roomId =',obj.RoomId);
+          if (name.toString().trim() === obj.RoomId.toString().trim()) 
+            return true;
+    }
+      return false;
+  }
 
   socket.on('calleejoin', function(name){
-    console.log('calleejoin');
+    console.log('====calleejoin====');
     //var socketIds = socketIdsInRoom(name);
     //callback(socketIds);
     
-    socket.join(name);
-    socket.room = name;
-
-    var socketIds = io.nsps['/'].adapter.rooms[name];
-    var collection = [];
-    for (var key in socketIds) {
-      console.log('key soceket : ',key);
-      collection.push(key);
-    }
-
-    //console.log('socketId =',socketId);
-    if(socketIds){
-      var obj = {
-        RoomId : name,
-        SocketId : collection[0],
-        Avaliable : 1
-      };
-    }
-    console.log('obj.roomId =',obj.RoomId);
-    console.log('obj.socketId =',obj.SocketId);
-    console.log('obj.avaliable =',obj.Avaliable);
-    SocketIdAvaiableArray.push(obj);
+    var duplecated  = checkRoomIdDuplicated(name);
     
-    for (var i in SocketIdAvaiableArray){
-      var obj = SocketIdAvaiableArray[i];
-      console.log('obj.roomId =',obj.RoomId);
-      console.log('obj.socketId =',obj.SocketId);
-      console.log('obj.avaliable =',obj.Avaliable);
+    if(!duplecated){
+        socket.join(name);
+        socket.room = name;
+
+        var socketIds = io.nsps['/'].adapter.rooms[name];
+        var collection = [];
+        for (var key in socketIds) {
+          console.log('key soceket : ',key);
+          collection.push(key);
+        }
+
+        if(socketIds){
+            console.log('new obj for stroe connectiong info!!');
+          var obj = {
+            RoomId : name,
+            RoomOwnerSocketId : collection[0],
+            Avaliable : 1,
+            CallerSocketId : null
+          };
+        }
+        console.log('obj.roomId =',obj.RoomId);
+        console.log('obj.socketId =',obj.RoomOwnerSocketId);
+        console.log('obj.avaliable =',obj.Avaliable);
+        SocketIdAvaiableArray.push(obj);
+        console.log('SocketIdAvaiableArray size = ',SocketIdAvaiableArray.length);
+        for (var i in SocketIdAvaiableArray){
+          var obj = SocketIdAvaiableArray[i];
+          console.log('obj.roomId =',obj.RoomId);
+          console.log('obj.socketId =',obj.RoomOwnerSocketId);
+          console.log('obj.avaliable =',obj.Avaliable);
+        }
+    }else{
+        console.log('room is duplecated!!!!!');
+        var to = io.sockets.connected[socket.id];
+        console.log('alert duplicated room id to socket.id = ', socket.id);
+        if(socket.id)
+            to.emit('duplicatedRoomId', socket.id);
     }
-    
     //var socketIds = io.nsps['/'].adapter.rooms[name];
     //console.log('socketIds: ', socketIds);
   });
@@ -130,14 +160,21 @@ io.on('connection', function(socket){
     console.log('caller join');
     //var socketIds = socketIdsInRoom(name);
     var socketIds = [];
+    console.log('SocketIdAvaiableArray size = ',SocketIdAvaiableArray.length);
     for (var i in SocketIdAvaiableArray){
       var obj = SocketIdAvaiableArray[i];
+        console.log('obj.RoomId = ',obj.RoomId);
+        console.log('obj.RoomOwnerSocketId = ',obj.RoomId);
+        console.log('obj.Avaliable = ',obj.Avaliable);
+        console.log('obj.CallerSocketId = ',obj.CallerSocketId);
       if(obj.Avaliable == 1){
-        socketId = obj.SocketId;
+        socketId = obj.RoomOwnerSocketId;
+        SocketIdAvaiableArray[i].CallerSocketId = socket.id;
         console.log('socketId =',socketId);
         socketIds.push(socketId);  
         socket.room = obj.RoomId;
         SocketIdAvaiableArray[i].Avaliable = 0;
+        break;
       }
     }
     callback(socketIds);
@@ -154,6 +191,7 @@ io.on('connection', function(socket){
   socket.on('exchange', function(data){
     console.log('exchange', data);
     data.from = socket.id;
+    console.log('exchange: to = ', data.to);
     console.log('exchange: from = ', data.from);
     var to = io.sockets.connected[data.to];
     console.log('exchange: to = ', data.to);
